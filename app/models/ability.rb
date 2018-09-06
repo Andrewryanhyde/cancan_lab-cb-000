b74dbf2013965a5f36962a805f3e4bae8b720d5d
Andrew Hyde <andrewryanhyde@hotmail.co.uk>
2018-09-06 09:22:34 +0000

    automatically backed up by learn

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -10,17 +10,6 @@ class Ability
     end
   end
 
-
-
-
-
-
-
-
-
-
-
-
     # Define abilities for the passed in user here. For example:
     #
     #   user ||= User.new # guest user (not logged in)
