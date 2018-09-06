99e7ea93bc8618105a7543d7cfeb81ea84ec114b
Andrew Hyde <andrewryanhyde@hotmail.co.uk>
2018-09-06 09:04:05 +0000

    automatically backed up by learn

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,49 +1,49 @@
 class NotesController < ApplicationController
 
-  def new 
-  end 
-
-  def create 
-    if logged_in? 
-      @note = current_user.notes.build(note_params)
-      if @note.save 
-        redirect_to '/'
-      else
-        redirect_to new_note_path 
-      end 
-    else 
-      redirect_to '/'
-    end 
+  def new
   end
 
-  def index 
-    @notes = Note.all 
-  end 
+  def create
+    if logged_in?
+      @note = current_user.notes.build(note_params)
+      if @note.save
+        redirect_to '/'
+      else
+        redirect_to new_note_path
+      end
+    else
+      redirect_to '/'
+    end
+  end
 
-  def show 
+  def index
+    @notes = Note.all
+  end
+
+  def show
     @note = Note.find_by_id(params[:id])
-  end 
+  end
 
-  def edit 
+  def edit
 
-  end 
+  end
 
-  def update 
+  def update
     @note = Note.find_by_id(params[:id])
     @note.update(note_params)
-    @note.save 
+    @note.save
     redirect_to '/'
-  end 
+  end
 
-  def destroy 
-  end 
+  def destroy
+  end
 
-  private 
+  private
 
   def note_params
     params.require(:note).permit(:content, :visible_to)
   end 
-    
+
 
 
 end
