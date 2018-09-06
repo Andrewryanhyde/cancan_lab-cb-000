f01f6ac42c6a709ed6993ae50b7171800b331d68
Andrew Hyde <andrewryanhyde@hotmail.co.uk>
2018-09-06 09:11:10 +0000

    automatically backed up by learn

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,10 +1,14 @@
 class Ability
   include CanCan::Ability
 
-  def initialize(user) 
+  def initialize(user)
     if user.present? #logged in users can manage their own posts 
-      can :manage, Note, user_id: user.id 
-    else 
+      can :manage, Note, user_id: user.id
+      can :read, Note do |note| 
+        note.readers.include? user
+      end
+    end
+  end
       
 
 
@@ -15,7 +19,8 @@ class Ability
 
 
 
-    
+
+
     # Define abilities for the passed in user here. For example:
     #
     #   user ||= User.new # guest user (not logged in)
