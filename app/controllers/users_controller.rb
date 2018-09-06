befc60d9ce66a639b7b9b1c13b49a9b2e2a7f971
Andrew Hyde <andrewryanhyde@hotmail.co.uk>
2018-09-06 09:36:07 +0000

    Done.

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,14 @@
+class UsersController < ApplicationController
+
+  def create
+    @user = User.new(:name => params[:name])
+    if @user.save
+      redirect_to notes_path
+    else
+      redirect_to root_path
+    end
+  end 
+
+
+
+end
