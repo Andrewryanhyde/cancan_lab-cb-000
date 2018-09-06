244827a1ff495d84c538b3120077a55bc61ac11c
Andrew Hyde <andrewryanhyde@hotmail.co.uk>
2018-09-06 09:19:32 +0000

    automatically backed up by learn

--- app/models/note.rb
+++ app/models/note.rb
@@ -3,26 +3,26 @@ class Note < ActiveRecord::Base
   has_many :viewers
   has_many :readers, through: :viewers, source: :user
 
-  before_save :ensure_owner_can_read 
+  before_save :ensure_owner_can_read
 
-  def visible_to 
+  def visible_to
     self.readers.map {|reader| reader.name }.join(", ")
-  end 
+  end
 
   def visible_to=(name_list)
-    name_list.split(", ").each do |name| 
+    name_list.split(", ").each do |name|
       reader = User.find_by(:name => name)
-      if reader 
-        self.readers << reader 
-      end 
-    end 
-    self.readers 
-  end 
+      if reader
+        self.readers << reader
+      end
+    end
+    self.readers
+  end
 
   def ensure_owner_can_read
     if user && !self.reader.include?(user)
-      readers << user 
-    end 
+      readers << user
+    end
   end 
-    
+
 end
