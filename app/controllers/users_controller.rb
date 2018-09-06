9d5bcb59f418edaf2739f2205391d0999abcf9b6
Andrew Hyde <andrewryanhyde@hotmail.co.uk>
2018-09-06 08:58:12 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,14 @@
 class UsersController < ApplicationController
 
-  def create 
+  def create
     @user = User.new(:name => params[:name])
-    if @user.save 
+    if @user.save
       redirect_to notes_path
-    else 
-      redirect_to root_path 
-    end 
+    else
+      redirect_to root_path
+    end
   end 
 
 
 
-end 
+end
