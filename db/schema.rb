99bafcf4e893860d11c6d50b0dd98a3d97d6ea1f
Andrew Hyde <andrewryanhyde@hotmail.co.uk>
2018-09-06 09:24:45 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180906084945) do
+ActiveRecord::Schema.define(version: 20180906092438) do
 
   create_table "notes", force: :cascade do |t|
     t.string   "content"
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
